cbe8eab9a89fd85614883d11db279580f56a7fb3
Juraj Piovar <jpiovar@sk.ibm.com>
2020-02-10 15:44:58 +0100

    added new component

---
+++ ngstore1/src/app/comp3/comp3.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from './../stores/app.state';
+import { Observable } from 'rxjs';
+import { Tutorial } from './../stores/tutorial/tutorial.model';
+
+@Component({
+  selector: 'app-comp3',
+  templateUrl: './comp3.component.html',
+  styleUrls: ['./comp3.component.scss']
+})
+export class Comp3Component implements OnInit {
+
+  tutorials: Observable<Tutorial[]>;
+
+  constructor(private store: Store<AppState>) {
+    this.tutorials = store.select('tutorial');
+  }
+
+  ngOnInit() {
+  }
+
+}
