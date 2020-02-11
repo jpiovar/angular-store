86002b4890b86fbfce3b1c9b0fa0d7f3c333aeb0
Juraj Piovar <jpiovar@sk.ibm.com>
2020-02-11 08:17:13 +0100

    subscription issue

--- ngstore1/src/app/comp1/comp1.component.ts
+++ ngstore1/src/app/comp1/comp1.component.ts
@@ -5,17 +5,22 @@ import { Tutorial } from '../stores/tutorial/types';
 import { AppState } from './../stores';
 import * as TutorialActions from '../stores/tutorial/actions';
 
+const myObserver = {
+  next: x => console.log('Observer got a next value: ' + x),
+  error: err => console.error('Observer got an error: ' + err),
+  complete: () => console.log('Observer got a complete notification'),
+};
+
 @Component({
   selector: 'app-comp1',
   templateUrl: './comp1.component.html',
   styleUrls: ['./comp1.component.scss']
 })
+
 export class Comp1Component implements OnInit {
 
-  // Section 1
   tutorials: Observable<Tutorial[]>;
 
-  // Section 2
   constructor(private store: Store<AppState>) {
     // tslint:disable-next-line: no-debugger
     debugger;
@@ -26,8 +31,8 @@ export class Comp1Component implements OnInit {
   get tut(): string {
     // tslint:disable-next-line: no-debugger
     debugger;
-    return '';
-    // return this.tutorials.subscribe(val => console.log(val));
+    // return '';
+    return this.tutorials.subscribe(myObserver);
   }
 
   delTutorial(index) {
